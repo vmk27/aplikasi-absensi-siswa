fb9695a29751a2c83814209698e687e6aca539fe
vmk27 <vpnunli001@gmail.com>
2025-07-18 00:01:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Penting! Memberitahu Tailwind untuk memindai index.html
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
